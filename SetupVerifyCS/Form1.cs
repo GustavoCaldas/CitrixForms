cc1774a4a6bb449e7e85d607c7156236591619f8
gustavo caldas <gustavocaldes@gmail.com>
2021-07-11 19:46:29 -0300

    Initial Setup

---
+++ SetupVerifyCS/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetupVerifyCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            string registry64_key = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key);
+            RegistryKey key64 = Registry.LocalMachine.OpenSubKey(registry64_key);
+
+            Helper helper = new Helper(key, key64);
+            helper.FindRequiredSoftwares();
+
+            if (helper.HasCitrix)
+                checkBox1.Checked = true;
+            if (helper.HasIDGo800Driver)
+                checkBox2.Checked = true;
+            if (helper.HasCitrixHDX)
+                checkBox3.Checked = true;
+            if (helper.HasZoomPlugin)
+                checkBox4.Checked = true;
+
+            if(!helper.HasCitrix ||
+                !helper.HasIDGo800Driver ||
+                !helper.HasCitrixHDX ||
+                !helper.HasZoomPlugin)
+            {
+                this.label5.Text = "Oh! You are missing some required softwares!";
+                this.label5.ForeColor = Color.Red;
+            }
+            else
+            {
+                this.label5.Text = "All Good!";
+                this.label5.ForeColor = Color.Green;
+            }
+        }
+    }
+}
---
+++ SetupVerifyCS/Helper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace SetupVerifyCS
+{
+    public sealed class Helper
+    {
+        public bool HasCitrix { get; private set; }
+        public bool HasIDGo800Driver { get; private set; }
+        public bool HasCitrixHDX { get; private set; }
+        public bool HasZoomPlugin { get; private set; }
+        public RegistryKey Key1 { get; private set; }
+        public RegistryKey Key2 { get; private set; }
+        public string[] Softwares { get; private set; }
+        public Helper(RegistryKey key1, RegistryKey key2)
+        {
+            this.Key1 = key1;
+            this.Key2 = key2;
+            this.HasCitrix = false;
+            this.HasIDGo800Driver = false;
+            this.HasCitrixHDX = false;
+            this.HasZoomPlugin = false;
+            this.Softwares = new string[]
+            {
+                "Citrix Workspace 1912",
+                "IDGo 800 Minidriver",
+                "Citrix HDX RealTime Media Engine 2.8",
+                "Zoom Plugin for Citrix Receiver"
+            };
+        }
+        public void FindRequiredSoftwares()
+        {
+            foreach (string subkey_name in Key1.GetSubKeyNames())
+            {
+                RegistryKey subkey = Key1.OpenSubKey(subkey_name);
+                if(subkey.GetValue("DisplayName") != null)
+                {
+                    if (subkey.GetValue("DisplayName").ToString().Contains(this.Softwares[0]))
+                        this.HasCitrix = true;
+
+                    if (subkey.GetValue("DisplayName").ToString().Contains(this.Softwares[1]))
+                        this.HasIDGo800Driver = true;
+
+                    if (subkey.GetValue("DisplayName").ToString().Contains(this.Softwares[2]))
+                        this.HasCitrixHDX = true;
+
+                    if (subkey.GetValue("DisplayName").ToString().Contains(this.Softwares[3]))
+                        this.HasZoomPlugin = true;
+                }
+            }
+            foreach (string subkey64_name in Key2.GetSubKeyNames())
+            {
+                RegistryKey subkey64 = Key2.OpenSubKey(subkey64_name);
+                if (subkey64.GetValue("DisplayName") != null)
+                {
+                    if (subkey64.GetValue("DisplayName").ToString().Contains(this.Softwares[0]))
+                        this.HasCitrix = true;                   
+
+                    if (subkey64.GetValue("DisplayName").ToString().Contains(this.Softwares[1]))
+                        this.HasIDGo800Driver = true;
+
+                    if (subkey64.GetValue("DisplayName").ToString().Contains(this.Softwares[2]))
+                        this.HasCitrixHDX = true;
+
+                    if (subkey64.GetValue("DisplayName").ToString().Contains(this.Softwares[3]))
+                        this.HasZoomPlugin = true;
+                }
+            }
+        }
+    }
+}
