821a800120ce90032d614242efffedb79a140d83
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-08-13 15:22:26 -0300

    Update UnitTest1.cs

--- TestSetupVerify/UnitTest1.cs
+++ TestSetupVerify/UnitTest1.cs
@@ -1,5 +1,3 @@
-using System;
-using Microsoft.Win32;
 using Xunit;
 
 namespace TestSetupVerify
@@ -12,36 +10,16 @@ namespace TestSetupVerify
             // Arrange
             // Act
             // Assert
-            Assert.True(true); 
+            Assert.True(true);
         }
 
-        [Fact(DisplayName = "Não passa")]
-        [Trait("Categoria", "Helper Trait Test")]
+        [Fact]
         public void Test2()
         {
             // Arrange
-            //string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-            //string registry64_key = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
-
-            //RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key);
-            //RegistryKey key64 = Registry.LocalMachine.OpenSubKey(registry64_key);
-
-            //Helper helper = new Helper(key, key64);
-            //helper.FindRequiredSoftwares();
-
             // Act
-            //bool var1 = helper.HasCitrix;
-            //bool var2 = helper.HasIDGo800Driver;
-            //bool var3 = helper.HasCitrixHDX;
-            //bool var4 = helper.HasZoomPlugin;
-            //bool var5 = helper.HasZoom;
-
             // Assert
-            //Assert.False(false);
-            //Assert.False(false);
-            //Assert.False(false);
-            //Assert.False(false);
-            Assert.False(false);
+            Assert.True(true);
         }
     }
 }
